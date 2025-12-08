99b21de8b49dab3b0407c065de665a50a3739581
Harin Lee <me@harin.net>
2025-12-08 15:57:19 +0900

    Add new MWAN route modes

--- src/main.c
+++ src/main.c
@@ -540,9 +540,17 @@ static PCatManagerRouteMode pcat_main_route_mode_by_string(const char *str) {
     {
         return PCAT_MANAGER_ROUTE_MODE_WIRED;
     }
+    else if(strcmp(str, "wireless")==0)
+    {
+        return PCAT_MANAGER_ROUTE_MODE_WIRELESS;
+    }
     else if(strcmp(str, "mobile")==0)
     {
         return PCAT_MANAGER_ROUTE_MODE_MOBILE;
+    }
+    else if(strcmp(str, "virtual")==0)
+    {
+        return PCAT_MANAGER_ROUTE_MODE_VIRTUAL;
     }
     else if(strcmp(str, "unknown")==0)
     {
@@ -710,6 +718,16 @@ static gboolean pcat_main_status_check_timeout_func(gpointer user_data)
                 if(g_pcat_main_net_status_led_work_mode)
                 {
                     pcat_pmu_manager_net_status_led_setup(
+                        100, 100, 0);
+                }
+
+                break;
+            }
+            case PCAT_MANAGER_ROUTE_MODE_WIRELESS:
+            {
+                if(g_pcat_main_net_status_led_work_mode)
+                {
+                    pcat_pmu_manager_net_status_led_setup(
                         50, 50, 0);
                 }
 
@@ -720,7 +738,17 @@ static gboolean pcat_main_status_check_timeout_func(gpointer user_data)
                 if(g_pcat_main_net_status_led_work_mode)
                 {
                     pcat_pmu_manager_net_status_led_setup(
-                        20, 380, 0);
+                        25, 25, 0);
+                }
+
+                break;
+            }
+            case PCAT_MANAGER_ROUTE_MODE_VIRTUAL:
+            {
+                if(g_pcat_main_net_status_led_work_mode)
+                {
+                    pcat_pmu_manager_net_status_led_setup(
+                        10, 10, 0);
                 }
 
                 break;
